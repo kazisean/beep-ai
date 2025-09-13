1346a2b2bedae78f35a7366449cdd6fb13d82ae2
kazisean <keh8423@nyu.edu>
2025-09-13 03:28:21 -0400

    base

---
+++ Beep/Beep/BeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeepApp.swift
+//  Beep
+//
+//  Created by hossain on 9/13/25.
+//
+
+import SwiftUI
+
+@main
+struct BeepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
